aafa6650814b3680e77622c8b8d3411d99230d35
blackflame666 <96531393+blackflame666@users.noreply.github.com>
2026-07-29 15:55:35 +0800

    Update print statement from 'Hello' to 'Goodbye'

--- main.py
+++ main.py
@@ -36,8 +36,7 @@ SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
-   # Create tables
-Base.metadata.create_all(bind=engine)
+   
 
 # --- DATABASE MODELS ---
 
